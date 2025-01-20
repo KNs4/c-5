5f77d56c336d5c6be44c5eac6e528aaeff941cfd
KNs4 <145314264+KNs4@users.noreply.github.com>
2025-01-20 10:44:52 +0500

    code.cpp

--- code.cpp
+++ code.cpp
@@ -54,24 +54,21 @@ public:
         text.close();
     }
 
-    // Задание 1: Исключить дубликаты
-    static void RemoveDuplicates(const string& inputFile, const string& outputFile) {
-        ifstream input(inputFile, ios::binary);
-        ofstream output(outputFile, ios::binary);
-        vector<int> numbers;
-        int num;
-        while (input.read(reinterpret_cast<char*>(&num), sizeof(num))) {
-            if (find(numbers.begin(), numbers.end(), num) == numbers.end()) {
-                numbers.push_back(num);
-                output.write(reinterpret_cast<char*>(&num), sizeof(num));
-            }
+    // Чтение содержимого текстового файла
+    static void ReadTextFile(const string& filename) {
+        ifstream file(filename);
+        string line;
+        while (getline(file, line)) {
+            cout << line << endl;
         }
-        input.close();
-        output.close();
+        cout << endl;
     }
 
-    // Задание 1: Исключить дубликаты в текстовом формате
-    static void RemoveDuplicatesText(const string& inputFile, const string& outputFile) {
+    // Задание 1: Исключить дубликаты
+    static void RemoveDuplicates(const string& inputFile, const string& outputFile) {
+        cout << "Начальный текстовый файл: " << endl;
+        ReadTextFile(inputFile);
+
         ifstream input(inputFile);
         ofstream output(outputFile);
         vector<int> numbers;
@@ -84,92 +81,100 @@ public:
         }
         input.close();
         output.close();
+
+        cout << "Конечный текстовый файл после удаления дубликатов: " << endl;
+        ReadTextFile(outputFile);
     }
 
     // Задание 4: Проверка наличия числа
-    static bool ContainsNumber(const string& filename, int target) {
+    static void ContainsNumber(const string& filename) {
+        // Сначала выводим содержимое файла
+        cout << "Начальный текстовый файл: " << endl;
+        ReadTextFile(filename);
+
+        // Ввод числа, которое хотим найти
+        cout << "Введите число, которое хотите найти в файле: ";
+        int target;
+        cin >> target; // Ввод числа с клавиатуры
+
         ifstream file(filename);
         int num;
+        bool found = false;
         while (file >> num) {
-            if (num == target) return true;
+            if (num == target) {
+                found = true;
+                break;
+            }
         }
-        return false;
+        file.close();
+
+        cout << "Результат поиска числа " << target << ": " << (found ? "найдено" : "не найдено") << endl;
     }
 
     // Задание 5: Сумма элементов, кратных числу
-    static int SumMultiples(const string& filename, int k) {
+    static void SumMultiples(const string& filename) {
+        cout << "Введите число, на которое должны быть кратны элементы: ";
+        int k;
+        cin >> k; // Ввод числа с клавиатуры
+
+        cout << "Начальный текстовый файл: " << endl;
+        ReadTextFile(filename);
+
         ifstream file(filename);
         int num, sum = 0;
         while (file >> num) {
             if (num % k == 0) sum += num;
         }
-        return sum;
+        file.close();
+
+        cout << "Сумма чисел, кратных " << k << " в файле '" << filename << "' = " << sum << endl;
     }
 
     // Задание 6: Строки без цифр
     static void CopyLinesWithoutDigits(const string& inputFile, const string& outputFile) {
+        cout << "Начальный текстовый файл: " << endl;
         ifstream input(inputFile);
-        ofstream output(outputFile);
         string line;
         while (getline(input, line)) {
-            if (none_of(line.begin(), line.end(), ::isdigit)) {
-                output << line << endl;
-            }
+            cout << line << endl;
         }
         input.close();
-        output.close();
-    }
 
-    // Записать ответ в файл
-    static void WriteToFile(const string& outputFile, const string& message) {
-        ofstream output(outputFile, ios::app);  // открытие для добавления в конец файла
-        output << message << endl;
-        output.close();
-    }
+        ifstream inputAgain(inputFile);
+        ofstream output(outputFile);
+        int linesCount = 0;
 
-    // Выводить сообщение в консоль и записывать в файл
-    static void OutputAndSave(const string& outputFile, const string& message) {
-        cout << message << endl;
-        WriteToFile(outputFile, message);
+        while (getline(inputAgain, line)) {
+            if (none_of(line.begin(), line.end(), ::isdigit)) {
+                output << line << endl;
+                linesCount++;
+            }
+        }
+        inputAgain.close();
+        output.close();
+
+        cout << "Конечный текстовый файл после копирования строк без цифр: " << endl;
+        ReadTextFile(outputFile);
+
+        cout << "Количество строк без цифр: " << linesCount << endl;
     }
 };
 
 int main() {
-    string outputFile = "results.txt";  // Файл для записи всех результатов
-
     // Задание 1: Генерация и удаление дубликатов
-    FileTasks::FillBinaryFile("binary1.dat", 10, 1, 100);
-    FileTasks::RemoveDuplicates("binary1.dat", "unique_binary.dat");
-    FileTasks::OutputAndSave(outputFile, "Задание 1 (Бинарный файл): Дубликаты удалены в файле 'unique_binary.dat'.");
-
     FileTasks::FillTextFileSingle("numbers.txt", 20, 1, 100);
-    FileTasks::RemoveDuplicatesText("numbers.txt", "unique_text.txt");
-    FileTasks::OutputAndSave(outputFile, "Задание 1 (Текстовый файл): Дубликаты удалены в файле 'unique_text.txt'.");
+    FileTasks::RemoveDuplicates("numbers.txt", "unique_text.txt");
 
-    // Задание 4: Ввод числа для поиска
-    cout << "Введите число для поиска в файле (Задание 4): ";
-    int numberToFind;
-    cin >> numberToFind;
-
+    // Задание 4: Генерация и проверка числа
     FileTasks::FillTextFileSingle("numbers_check.txt", 20, 1, 100);
-    bool contains = FileTasks::ContainsNumber("numbers_check.txt", numberToFind);
-    FileTasks::OutputAndSave(outputFile, "Задание 4: Число " + to_string(numberToFind) + " " + 
-        (contains ? "найдено" : "не найдено") + " в файле 'numbers_check.txt'.");
+    FileTasks::ContainsNumber("numbers_check.txt");
 
-    // Задание 5: Ввод числа для кратности
-    cout << "Введите число, для которого нужно найти сумму кратных чисел (Задание 5): ";
-    int multipleCheck;
-    cin >> multipleCheck;
-
+    // Задание 5: Генерация и вычисление суммы
     FileTasks::FillTextFileMultiple("numbers_multi.txt", 5, 4, 1, 100);
-    int sumMultiples = FileTasks::SumMultiples("numbers_multi.txt", multipleCheck);
-    FileTasks::OutputAndSave(outputFile, "Задание 5: Сумма чисел, кратных " + to_string(multipleCheck) + 
-        " в файле 'numbers_multi.txt' = " + to_string(sumMultiples) + ".");
+    FileTasks::SumMultiples("numbers_multi.txt");
 
     // Задание 6: Генерация и копирование строк без цифр
     FileTasks::CopyLinesWithoutDigits("text.txt", "no_digits.txt");
-    FileTasks::OutputAndSave(outputFile, "Задание 6: Строки без цифр скопированы в файл 'no_digits.txt'.");
 
     return 0;
 }
-
